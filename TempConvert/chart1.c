ded7bfab4e243bc0b23e18f0ea42610e8db0c1d9
Kyle McCoy <krm7269@rit.edu>
2019-01-23 15:37:10 -0500

    Fixed chart1.c

--- TempConvert/chart1.c
+++ TempConvert/chart1.c
@@ -3,13 +3,14 @@
 
 int main(){
 	int fahren = 0;
-	int cel = 0;
+	int cel;
 	
 	printf( "Fahrenheit - Celsius\n" );
 
 	while( fahren <= 300 ){
-		cel = ((5/9) * ((fahren) - 32));
-		printf("\t%d\t%d\n", fahren, cel);
+		cel = (5.0/9.0)* (fahren-32);
+		printf("\t%d\t", fahren);
+		printf("%d\n", cel);
 		fahren = fahren + 20;
 	}
 	return 0;
