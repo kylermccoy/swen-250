91eea726b22f97dfd5f248d1f1436975f4dbb3ba
Kyle McCoy <krm7269@rit.edu>
2019-01-29 16:45:12 -0500

    Fixed "prime_standard.c" to proper standards

--- c-debug/prime_standard.c
+++ c-debug/prime_standard.c
@@ -9,47 +9,62 @@ WARNING: There are bugs in this program! */
 
 #include <stdio.h>
 
-int Prime[15];  /* Prime[i] will be 1 if i is prime, 0 otherwise */
-int UpperBound; /* check all numbers up through this one for primeness */
+int primes[15];  /* prime[i] will be 1 if i is prime, 0 otherwise */
+int upper_bound; /* check all numbers up through this one for primeness 
+*/
 
-void CheckPrime(int K, int Prime[]) {
-  int J;
+/*
+   Check_Prime() function checks the primality of the number
+   Parameters: 
+	int k : number to check if divisible
+	int primes[] : array containing 1 is [i] prime or 0 if [i] is not prime
+   No Output
+*/
+void Check_Prime(int k, int primes[]) {
+  int j;
 
-  /* the plan:  see if J divides K, for all values J which are
-  themselves prime (no need to try J if it is nonprime), and
-  less than or equal to sqrt(K) (if K has a divisor larger
+  /* the plan:  see if j divides k, for all values j which are
+  themselves prime (no need to try j if it is nonprime), and
+  less than or equal to sqrt(k) (if k has a divisor larger
   than this square root, it must also have a smaller one,
   so no need to check for larger ones) */
  
-  J = 2;
-  while (J * J <= K) {
-    if (Prime[J] == 1){
-      if (K % J == 0)  {
-        Prime[K] = 0;
+  j = 2;
+  while (j * j <= k) {
+    if (primes[j] == 1){
+      if (k % j == 0)  {
+        primes[k] = 0;
         return;
-      } /* if (K % J == 0) */
-    } /* if (Prime[J] == 1) */
-    J++;
-  } /* while (J * J <= K) */
+      } /* if (k % j == 0) */
+    } /* if (primes[j] == 1) */
+    j++;
+  } /* while (j * j <= k) */
 
-  /* if we get here, then there were no divisors of K, so it is prime */
-  Prime[K] = 1;
+  /* if we get here, then there were no divisors of k, so it is prime */
+  primes[k] = 1;
 
-}  /* CheckPrime() */
+}  /* Check_Prime() */
 
+/*
+   main() function asks user for an upperbound and loops
+   through the numbers checking for primality
+   No parameters
+   Outputs the numbers found to be prime
+*/
 int main() {
   int i;
-
+	
+  // Access uperbound
   printf("Enter upper bound:\n");
-  scanf("%d", &UpperBound);
-  UpperBound = 50 ;
-  Prime[1] = 1;
-  Prime[2] = 1;
-  for (i = 3; i <= UpperBound; i += 2) {
-    CheckPrime(i, Prime);
-    if (Prime[i]) {
+  scanf("%d", &upper_bound);
+  primes[1] = 1;
+  primes[2] = 1;
+  // Loop through numbers checking primality until upper_bound
+  for (i = 3; i <= upper_bound; i += 2) {
+    Check_Prime(i, primes); // calls function to check primality
+    if (primes[i]) {
       printf("%d is a prime\n", i);
-    } /* if (Prime[i]) */
-  } /* for (i = 3; i <= UpperBound; i += 2) */
+    } /* if (primes[i]) */
+  } /* for (i = 3; i <= upper_bound; i += 2) */
   return 0;
 } /* main() */
