4ae8395474beb031305ab51b5d96642a2fd359a1
Kyle McCoy <krm7269@rit.edu>
2019-03-21 14:43:48 -0400

    completed test cases, updated ActivityJournal

--- analysis_part2/unit_tests_student.c
+++ analysis_part2/unit_tests_student.c
@@ -33,12 +33,30 @@ int test_student()
 		? passcount++ : failcount++ ;
 
 	// Add a test to verify that the word "day" is used three times in the list.
-	
+	result = find_word(&list1, "day") ;
+	int count = list1.p_current->one_word.word_count ;
+	assert( count==3,
+		"2 find_word searched for \"day\". Expect word count of 3 but received %d.", count )
+		? passcount++ : failcount++ ;
+
 	// Add a test to verify that the word AFTER the word "have" is the word "heat".
-	
+	result = find_word(&list1,"have") ;
+	struct word_entry test = get_next_word(&list1) ;
+	assert( strcmp(test.unique_word, "heat")==0,
+		"3 get_next_word searched for the word after \"have\". Expect \"heat\" but received %s.", test.unique_word)
+		? passcount++ : failcount++ ;
+
 	// Add a test to verify that the last word in the list is "with"
+	test = get_last_word(&list1) ;
+	assert( strcmp(test.unique_word, "with")==0 ,
+		"4 get_last_word searched for the last word in the list. Expect \"with\" but received %s.", test.unique_word)
+		? passcount++ : failcount++ ;
 	
 	// Add a test to verify that the next to the last word is "will"	
+	test = get_prev_word(&list1) ;
+	assert( strcmp(test.unique_word, "will")==0 ,
+		"5 get_prev_word searched for the second to last word in the list. Expect \"will\" but received %s.", test.unique_word)
+		? passcount++ : failcount++ ;
 
 	result = clear_linked_list( &list1 ) ;
 	assert( result == 62,
