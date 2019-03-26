b64695e8c359ed0a02ef4850ea3a737c9b503de6
Kyle McCoy <krm7269@rit.edu>
2019-03-26 16:10:19 -0400

    completed cpracticum2

--- cpracticum2/linked_strings.c
+++ cpracticum2/linked_strings.c
@@ -17,7 +17,17 @@
 void push(char *name) {
 
 	/* YOUR CODE HERE */
-	
+	node *created = malloc(sizeof(node)) ;
+	created->name = malloc(strlen(name)+1) ; 
+	strlcpy(created->name,name,strlen(name)+1) ;
+	if(pHead == NULL){
+		pHead = created ;
+		pHead->pNext = NULL ;
+	}else{
+		node *oldHead = pHead ;
+		pHead = created ;
+		pHead->pNext = oldHead ;
+	}
 }
 
 /************************************************************
@@ -30,9 +40,13 @@ void push(char *name) {
 char* pop() {
 
 	/* YOUR CODE HERE */
-
-
-	return NULL; //Just to allow compile; needs to be fixed
+	char *returned = malloc(strlen(pHead->name)+1) ;
+	strlcpy(returned,pHead->name,strlen(pHead->name)+1) ;
+	node *oldHead = pHead ;
+	pHead = pHead->pNext ;
+	free(oldHead->name) ;
+	free(oldHead) ;
+	return returned ;
 }
 
 /************************************************************
@@ -45,8 +59,12 @@ void print_list() {
 	printf("List:\n");
 
 	/* YOUR CODE HERE */
-
-
+	node *temp = pHead ;
+	while(temp != NULL){
+		printf("%s ",temp->name) ;
+		temp = temp->pNext ;
+	}
+	printf("\n") ;
 }
 
 /************************************************************
@@ -57,7 +75,9 @@ void print_list() {
 void clear(){
 
 	/* YOUR CODE HERE */
-
+	while(pHead != NULL){
+		free(pop()) ;
+	}
 
 }
 
@@ -75,11 +95,17 @@ void clear(){
  ***********************************************************/
 void reverse_list()
 {
-  node* current = pHead; //pHead declared in linked_strings.h
-  node* next = NULL; 
-  node* prev = NULL;
-  /* YOUR CODE HERE */
-
- 
+	node* current = pHead; //pHead declared in linked_strings.h
+	node* next = NULL ; 
+	node* prev = NULL;
+	/* YOUR CODE HERE */
+	next = current ;
+	while(next != NULL){
+		next = current->pNext ;
+		current->pNext = prev ;
+		prev = current ;
+		current = next ;
+	}
+ 	pHead = prev ;
 }
 
