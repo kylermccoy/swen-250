c52512e1c669e9fa29d4e3794f5303cfd8ad2e20
Kyle McCoy <krm7269@rit.edu>
2019-04-09 15:38:46 -0400

    setup for ruby activity

---
+++ Factorial/factorial.rb
@@ -0,0 +1,8 @@
+# factorial(n) is defined as n*n-1*n-2..1 for n > 0
+# factorial(n) is 1 for n=0
+# Let's raise an exception if factorial is negative
+# Let's raise an exception if factorial is anything but an integer
+
+def factorial(n)
+  # Write the factorial code here per the activity
+end
---
+++ Factorial/factorial_test.rb
@@ -0,0 +1,17 @@
+require_relative 'factorial' #We need to access our code to test
+require 'minitest/autorun'          #We need Ruby's unit testing library
+
+class FactorialTest < MiniTest::Test #This is a class. (It's ok if you don't know what those are yet)
+
+  #Test methods MUST start with test_
+  def test_normal
+    assert_equal 24, factorial(4),"4! should be 24"
+  end
+
+  # Add many other test methods here
+  #
+  def test_another_normal
+    assert_equal 120, factorial(5), "5! should be 120"
+  end
+
+end
