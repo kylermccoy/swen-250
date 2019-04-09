703ce40d17f9ebe7a686fe36781d047d04a633dc
Kyle McCoy <krm7269@rit.edu>
2019-04-09 14:30:02 -0400

    fixed histogram2

--- RubyHistogram/histogram2.rb
+++ RubyHistogram/histogram2.rb
@@ -1,6 +1,15 @@
 $stdin.each do |line|
-        # process this line
+	# process this line
+line.chomp!
+line.downcase!
+line.gsub(/[^a-zA-Z\s]/, '')
+line.sub(/^\s+/, '')
 words = line.split(/ +/)
 bag = Hash.new(0)
 words.each {|x| bag[x] += 1}
+puts "Printing from Hash!"
+bag.each {|key, value| puts "#{key}\t#{value}"}
+arr = bag.select {|key, value| value > 1}
+puts "Printing from Array!"
+arr.each {|key, value| puts "#{key}\t#{value}"}  
 end
